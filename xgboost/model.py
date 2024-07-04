0f402cc63a4a193b9af1278c4f1adaa4a7a876c3
mmtzlares <maumtzlares@gmail.com>
2024-07-03 22:18:40 -0600

    add mlflow logs

--- xgboost/model.py
+++ xgboost/model.py
@@ -5,6 +5,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
+# Set MLflow tracking URI
+mlflow.set_tracking_uri("http://localhost:5001")
+
 # Get the wine quality dataset from the UCI repo
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
 data = pd.read_csv(url, delimiter=";")
@@ -13,9 +16,6 @@ data = pd.read_csv(url, delimiter=";")
 X = data.drop("quality", axis=1)
 y = data["quality"]
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Set MLflow tracking URI
-mlflow.set_tracking_uri("http://localhost:5001")
 
 
 # Retrieve the ID of an existing MLflow experiment or create a new one if it doesn't exist.
@@ -30,7 +30,6 @@ experiment_id = get_or_create_experiment("XGBoost Example")
 
 # Set the current active MLflow experiment
 mlflow.set_experiment(experiment_id=experiment_id)
-
 mlflow.xgboost.autolog()
 
 
@@ -38,6 +37,7 @@ mlflow.xgboost.autolog()
 def objective(trial):
     params = {
         "objective": "reg:squarederror",
+        "eval_metric": "rmse",
         "n_estimators": 1000,
         "verbosity": 0,
         "learning_rate": trial.suggest_float("learning_rate", 1e-3, 0.1, log=True),
@@ -57,4 +57,15 @@ def objective(trial):
 study = optuna.create_study(direction="minimize")
 study.optimize(objective, n_trials=15)
 
-mlflow.last_active_run()
+# Fetching best parameters and the best RMSE score
+best_params = study.best_params
+best_rmse = study.best_value
+
+# Optionally, you can retrain the model with the best parameters and log the model
+model = xgb.XGBRegressor(**best_params)
+model.fit(X_train, y_train, verbose=False)
+mlflow.xgboost.log_model(model, "model")
+
+# Get the logged model URI
+model_uri = mlflow.get_artifact_uri("model")
+print("Model URI:", model_uri)
